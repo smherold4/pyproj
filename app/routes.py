79ac5035a0ff4e1d5c9ecbe53da1cd3e4e415873
Mike Herold <herold4@gmail.com>
2025-08-17 23:01:00 -0400

    save

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
+from datetime import datetime
 from flask import Blueprint, redirect, url_for, render_template, request, flash
 from app.models.job import Job
 from app import db
-# from .tasks import insert_job
+from app.tasks.process_job import process_job
 
 bp = Blueprint("routes", __name__)
 
@@ -12,12 +13,16 @@ def root():
 @bp.route("/go", methods=["GET", "POST"])
 def go():
     if request.method == "POST":
-        # insert_job.delay()
-        flash("Enqueued a job!")
+        job = Job(created_at=datetime.now())
+        db.session.add(job)
+        db.session.commit()
+        process_job.delay(job.id)
+        flash("Enqueued job {}".format(job.id))
         return redirect(url_for("routes.go"))
     return render_template("go.html")
 
 @bp.route("/check")
 def check():
     count = db.session.query(Job).count()
-    return render_template("check.html", count=count)
+    count_processed = db.session.query(Job).filter(Job.processed_at.isnot(None)).count()
+    return render_template("check.html", count=count, count_processed=count_processed)
